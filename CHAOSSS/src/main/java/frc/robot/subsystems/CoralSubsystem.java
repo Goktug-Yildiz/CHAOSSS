18275191e32cfe49d8c898c9e7588e60b30a8043
Göktuğ Yıldız <cspro1505@gmail.com>
2025-02-21 10:00:27 +0300

    SWERVE FINAL

---
+++ CHAOSSS/src/main/java/frc/robot/subsystems/CoralSubsystem.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+public class CoralSubsystem {
+    private final VictorSPX
+
+    coralMotor = new VictorSPX(16);
+
+    public void coralDrop(double speed){
+        coralMotor.set(ControlMode.PercentOutput, speed);
+        System.out.println("Dropping the coral");
+    }
+    
+    public void stopCoral() {
+        coralMotor.set(ControlMode.PercentOutput, 0); // Stop the coral motor
+    }
+
+    public Command coralDrop(){
+        return new Command(){
+            @Override
+            public void initialize(){
+                coralDrop(0.8);
+                System.out.println("Dropping the coral");
+            }
+            @Override
+            public boolean isFinished(){
+                return true;
+            }
+        };
+    }
+}
---
+++ CHAOSSS/src/main/java/frc/robot/subsystems/ShooterSubsystem.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+
+public class ShooterSubsystem {
+    private final VictorSPX
+
+    shooterMotor = new VictorSPX(15);
+
+    public void shootBall(double speed){
+        shooterMotor.set(ControlMode.PercentOutput, speed);
+        System.out.println("Shooting the ball");
+    }
+    
+    public void stopShooter() {
+        shooterMotor.set(ControlMode.PercentOutput, 0); // Stop the shooter motor
+    }
+    public Command shootBall() {
+        return new Command() {
+            @Override
+            public void initialize() {
+                shootBall(0.8);
+                System.out.println("Shooting the ball");
+            }
+
+            @Override
+            public boolean isFinished() {
+                return true;
+            }
+        };
+    }
+}
